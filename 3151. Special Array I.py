8f8176e78ce30c880f1738d01d059151271779e5
Asrayev Dilshod <dasraev@Asrayevs-MacBook-Air.local>
2024-06-24 14:31:43 +0500

    1913, 3151

--- 3151. Special Array I.py
+++ 3151. Special Array I.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isArraySpecial(self, nums: List[int]) -> bool:
+    def isArraySpecial(self, nums) -> bool:
         if len(nums)==1:
             return True
         for i in range(len(nums)-1):
